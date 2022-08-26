de12cd28ef2e8dee0e46a304dfdf427da814bce9
renatomateusx <renatomateusx@gmail.com>
2022-08-26 17:22:33 -0300

    Initial Commit

---
+++ StriderChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StriderChallenge
+//
+//  Created by Renato Mateus on 26/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
